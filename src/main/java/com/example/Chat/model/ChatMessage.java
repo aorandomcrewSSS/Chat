80bd203258015dc74e884b2ab6271d28769e86bc
aorandomcrewSSS <danbogomolovhr@gmai.com>
2024-12-26 00:29:53 +0300

    Simple Web-Socket Application

---
+++ src/main/java/com/example/Chat/model/ChatMessage.java
@@ -0,0 +1,18 @@
+package com.example.Chat.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+
+    private String sender;
+
+    private MessageType type;
+
+}
---
+++ src/main/java/com/example/Chat/model/MessageType.java
@@ -0,0 +1,7 @@
+package com.example.Chat.model;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
